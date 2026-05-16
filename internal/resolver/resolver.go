d3b676dd1a31fbae3ab7bbefc00745e5cf2d2e9d
Farshid Mousavi <r.shidef@gmail.com>
2026-05-16 21:34:12 +0330

    fix: increment QueriesGlobal on GlobalDNS-first path in resolveWithLearning

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -201,6 +201,7 @@ func (r *Resolver) resolveWithLearning(req *dns.Msg, domain string) *dns.Msg {
 			}
 		case <-waitTimer.C:
 		}
+		r.metrics.QueriesGlobal.Add(1)
 		r.log.Info("routed", "domain", domain, "upstream", "global")
 		return globalMsg
 	}
